ffd92918ed336924320884784287ab73740fac46
Ingemar Ceicer <ingcei0123@lerum.local>
2019-10-24 13:57:27 +0200

    Konstigt

---
+++ klass/myfirstklass/minklass.h
@@ -0,0 +1,17 @@
+
+class Minklass
+{
+	// saker man endast kommer åt från klassen
+	private:
+		float summa;
+		
+		
+		
+		
+		
+	// saker man kommer åt från main	
+	public:
+		void setSumma(float);
+		float getSumma();
+	
+};
